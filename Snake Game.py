31a972acfe2c68e148b750b5e7b4d5ae40468be6
YAMAN ALAHMAD <yamana192@gmail.com>
2024-05-14 17:11:03 +0300

    first version

---
+++ Snake Game.py
@@ -0,0 +1,113 @@
+import pygame, sys, random
+from pygame import Vector2
+
+pygame.init()  # pygame start
+
+
+class SNAKE:
+    def _init_(self):
+        self.body = [Vector2(5, 10), Vector2(4, 10), Vector2(3, 10)]
+        self.direction = Vector2(1, 0)
+        self.new_block = False
+
+    def draw_snake(self):
+        for block in self.body:
+            x_pos = int(block.x * cell_size)
+            y_pos = int(block.y * cell_size)
+            block_rect = pygame.Rect(x_pos, y_pos, cell_size, cell_size)
+            pygame.draw.rect(screen, (183, 111, 122), block_rect)
+
+    def move_snake(self):
+        if self.new_block == True:
+            body_copy = self.body[:]
+            body_copy.insert(0, body_copy[0] + self.direction)
+            self.body = body_copy[:]
+            self.new_block = False
+        else:
+            body_copy = self.body[:-1]
+            body_copy.insert(0, body_copy[0] + self.direction)
+            self.body = body_copy[:]
+
+
+class FRUIT:
+    def _init_(self):
+        self.randomize()
+
+    def draw_fruit(self):
+        fruit_rect = pygame.Rect(int(self.pos.x * cell_size), int(self.pos.y * cell_size), cell_size, cell_size)
+        screen.blit(apple, fruit_rect)
+        # pygame.draw.rect(screen, (126, 166, 114), fruit_rect)
+
+    def randomize(self):
+        self.x = random.randint(0, cell_number - 1)  # rand places of the fruit
+        self.y = random.randint(0, cell_number - 1)
+        self.pos = Vector2(self.x, self.y)
+
+
+class MAIN:
+    def _init_(self):
+        self.snake = SNAKE()
+        self.fruit = FRUIT()
+
+    def update(self):
+        self.snake.move_snake()
+        self.check_collision()  # calling the method of checking on the fruit if it is eaten
+        self.check_fail()  # callign a method that tells if the snake hit the wall
+
+    def draw_elements(self):
+        self.fruit.draw_fruit()
+        self.snake.draw_snake()
+
+    def check_collision(self):
+        if self.fruit.pos == self.snake.body[0]:  # check if the snake is eating fruit
+            self.fruit.randomize()
+            self.snake.new_block = True
+
+    def check_fail(self):
+        if not 0 <= self.snake.body[0].x < cell_number or not 0 <= self.snake.body[0].y < cell_number:
+            self.game_over()
+
+        for block in self.snake.body[1:]:
+            if block.x == self.snake.body[0].x and block.y == self.snake.body[0].y:
+                self.game_over()
+
+    def game_over(self):
+        pygame.quit()
+        sys.exit()
+
+
+cell_size = 40
+cell_number = 20
+screen = pygame.display.set_mode((cell_number * cell_size, cell_size * cell_number))  # window
+apple = pygame.image.load('/Users/omaraldowir/Desktop/Graphics/apple.png').convert_alpha()
+clock = pygame.time.Clock()  # consist frames = 60
+main_game = MAIN()
+SCREEN_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SCREEN_UPDATE, 150)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == SCREEN_UPDATE:
+            main_game.update()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                if main_game.snake.direction.y != 1:
+                    main_game.snake.direction = Vector2(0, -1)
+            if event.key == pygame.K_DOWN:
+                if main_game.snake.direction.y != -1:
+                    main_game.snake.direction = Vector2(0, 1)
+            if event.key == pygame.K_RIGHT:
+                if main_game.snake.direction.x != -1:
+                    main_game.snake.direction = Vector2(1, 0)
+            if event.key == pygame.K_LEFT:
+                if main_game.snake.direction.x != 1:
+                    main_game.snake.direction = Vector2(-1, 0)
+
+    screen.fill((175, 215, 70))
+    main_game.draw_elements()
+    pygame.display.update()
+
+    clock.tick(60)
---
+++ chapter2.py
@@ -0,0 +1,38 @@
+# print("123")
+# print("Hello World")
+# x = "Moayad"
+# y = 18
+# x = 100
+# print(x)
+# z = 35.0
+# y = 12.50
+# a = z * y
+# print(a)
+
+# x = 2
+# x = x + 2
+# print(x)
+#
+# eee = 'hello ' + 'there'
+# # eee = eee + 1
+# print(type(eee))
+# # <class'str'>
+# print(type('hello'))
+# # <class'str'>
+# print(type(1))
+# # <class'int'>
+#
+# name = input('Enter your name: ')
+# print("Hello " + name)
+
+
+# inp = input('Europe floor?')
+# usf = int(inp) + 1
+# print('US floor', usf)
+
+##Exercise
+hours = int(input("Enter Hours: "))
+rate = float(input("Enter Rate: "))
+
+pay = hours * rate
+print("Pay: ",pay)
